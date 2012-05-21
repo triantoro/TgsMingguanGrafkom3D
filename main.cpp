0bfc0f6585ae3596c3effa0cdc783033290ca719
Arif Rahman <bra_kikuk@yahoo.com>
2012-05-22 03:13:08 +0700

    Penambahan warna dan Pencahayaan

--- main.cpp
+++ main.cpp
@@ -15,8 +15,23 @@ IF-10
 
 
 void init(void){
-	glClearColor(0.0,0.0,0.0,0.0);
-	glShadeModel(GL_FLAT);
+    GLfloat mat_specular[] = { 1.0, 0.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 35.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat light_position2[] = { -2.0, 1.0, 1.0, 0.0 };
+	GLfloat white_light[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat lmodel_ambient[] = { 0.1, 0.1, 0.1, 1.0 };
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+     
+     
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
